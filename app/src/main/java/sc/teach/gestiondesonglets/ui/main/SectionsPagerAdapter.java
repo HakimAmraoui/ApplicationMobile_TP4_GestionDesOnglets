92285ddff9be18d4781d6db4f1c2c5dd898ee74d
Hakim <hakim.amraoui@outlook.fr>
2021-03-23 18:35:11 +0100

    Initial commit

---
+++ app/src/main/java/sc/teach/gestiondesonglets/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,67 @@
+package sc.teach.gestiondesonglets.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.Locale;
+
+import sc.teach.gestiondesonglets.R;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        switch (position) {
+            case 0:
+                return NatureFragment.newInstance(0, mContext.getString(R.string.titre_section0));
+            case 1:
+                return NatureFragment.newInstance(1, mContext.getString(R.string.titre_section1));
+            case 2:
+                return NatureFragment.newInstance(2, mContext.getString(R.string.titre_section2));
+        }
+        return null;
+    }
+
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        Locale l = Locale.getDefault();
+        switch (position) {
+            case 0:
+                return mContext.getString(R.string.titre_section0).toUpperCase(l);
+            case 1:
+                return mContext.getString(R.string.titre_section1).toUpperCase(l);
+            case 2:
+                return mContext.getString(R.string.titre_section2).toUpperCase(l);
+        }
+        return null;
+    }
+
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 3;
+    }
+}
